ec960d7c0ebf1dbe522f4d61a8fc9125dd85a77c
realkevinbrian <kevinbrian105@gmial.com>
2021-06-22 05:00:12 -0700

    onscoll feature added

--- assets/script/App.js
+++ assets/script/App.js
@@ -1,12 +1,14 @@
-let typeWriter = document.getElementById("typeWriter");
-let text = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maxime ";
+var i = 0;
+let name = "Kevin Brian";
+var txt = `Hey, my name is ${name}, I am a Web Developer, Welcome to my portolio`; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
 
-
-for(let i=0 ; i < text.length; i++){
-
-    typeWriter.innerHTML += text.charAt(i);
-
-    setTimeout()
-
-
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typeWriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
+
+typeWriter();
