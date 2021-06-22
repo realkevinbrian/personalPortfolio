b5080e7c5991ff544b628864e77b428d71122a8e
realkevinbrian <kevinbrian105@gmial.com>
2021-06-22 04:23:22 -0700

    adding typewriter feature

---
+++ assets/script/App.js
@@ -0,0 +1,12 @@
+let typeWriter = document.getElementById("typeWriter");
+let text = " Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae maxime ";
+
+
+for(let i=0 ; i < text.length; i++){
+
+    typeWriter.innerHTML += text.charAt(i);
+
+    setTimeout()
+
+
+}
