bdb67b3c0a332d482f9a54e6a6a75b5efe4c2e97
realkevinbrian <kevinbrian105@gmial.com>
2021-06-27 15:09:07 -0700

    UI interface Updating

--- assets/script/App.js
+++ assets/script/App.js
@@ -1,12 +1,12 @@
 window.onload = typeWriter;
 var i = 0;
 let name = "Kevin Brian";
-var txt = `Hey, my name is ${name},I create stylish and functional website!`; /* The text */
+var txt = `Hello, I'm ${name}, Nice to meet you!`; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("typeWriter").innerHTML += txt.charAt(i);
+    document.getElementById("typeWrite").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
